0e09ef63c4d0b40a27ab6251acbfc3fcef1cc26a
0xwb7 <woobin200111@gmail.com>
2025-09-23 15:49:31 +0900

    25-26-Server-Assignment-01_이우빈

--- src/woobin/StudentDetailImpl.java
+++ src/woobin/StudentDetailImpl.java
@@ -3,7 +3,7 @@ package woobin;
 import java.util.*;
 
 public class StudentDetailImpl implements StudentDetail {
-    Map<Integer, Student> studentsMap = new HashMap<>();
+    private final Map<Integer, Student> studentsMap = new HashMap<>();
 
     @Override
     public boolean addStudent(int id, Student student) {
@@ -36,39 +36,31 @@ public class StudentDetailImpl implements StudentDetail {
         return studentsMap.size();
     }
 
-
     @Override
     public List<String> findStudentsByAgeMoreThan(int age) {
-        List<String> list = new ArrayList<>();
-        studentsMap.values().stream()
+        return studentsMap.values().stream()
                 .filter(student -> student.getAge() >= age)
-                .forEach(student -> list.add(student.getName()));
-
-        return list;
+                .map(Student::getName)
+                .toList();
     }
 
     @Override
     public Optional<Student> findStudentByPart(String part) {
-        Optional<Student> student = Optional.empty();
-        studentsMap.values().stream()
-                .filter(s -> s.getPart().equals(part))
-                .limit(1);
-        return student;
+        return studentsMap.values().stream()
+                .filter(student -> student.getPart().equals(part))
+                .findAny();
     }
 
     @Override
     public List<Student> findAllStudents() {
-        List<Student> list = new ArrayList<>();
-        studentsMap.values().stream()
+        return studentsMap.values().stream()
                 .sorted(Comparator.comparing(Student::getBirthday))
-                .forEach(student -> list.add(student));
-
-        return list;
+                .toList();
     }
 
     @Override
     public void printStudents() {
-        studentsMap.values().stream().forEach(student
-                -> System.out.println("이름: " + student.getName() + ", 나이: " + student.getAge() + ", 파트: " + student.getPart() + ", 생일: " + student.getBirthday()));
+        studentsMap.values()
+                .forEach(System.out::println);
     }
 }
